37f0026105dc73f35d974af89e3007865cab4297
Tchilo <tchiloross@gmail.com>
2021-10-27 20:36:54 +0200

    Make functional component class component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,32 +1,39 @@
 import React from 'react';
 
-function Calculator() {
-  return (
-    <div>
-      <div className="screen">0</div>
-      <div className="calculatorKeys">
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="orange">+</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="orange">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="zero">0</button>
-        <button type="button">.</button>
-        <button type="button" className="orange">=</button>
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="screen">0</div>
+        <div className="calculatorKeys">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="orange">÷</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="orange">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="orange">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="orange">+</button>
+          <button type="button" className="zero">0</button>
+          <button type="button">.</button>
+          <button type="button" className="orange">=</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Calculator;
