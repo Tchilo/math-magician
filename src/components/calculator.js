47c2757cade337b975c5eed9ecdf28c3ce2f798d
Tchilo <tchiloross@gmail.com>
2021-10-27 19:28:46 +0200

    Add component file and create calculator body

---
+++ src/components/calculator.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Calculator() {
+  return (
+    <div>
+      <div className="screen">0</div>
+      <div className="calculatorKeys">
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="orange">+</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="orange">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="orange">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="orange">+</button>
+        <button type="button" className="zero">0</button>
+        <button type="button">.</button>
+        <button type="button" className="orange">=</button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
