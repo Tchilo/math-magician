c6eaaf491aa5b9216e402a8d06851fdc08f8a208
Tchilo <tchiloross@gmail.com>
2021-10-29 18:14:39 +0200

    Refactor calculator class component  to function component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,55 +1,48 @@
 /* eslint-disable react/no-unused-state */
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      next: null,
-      total: null,
-    };
+function Calculator() {
+  const [state, setState] = useState({
+    next: null,
+    total: null,
+  });
 
-    this.handler = this.handler.bind(this);
+  function handler(e) {
+    setState({ ...state, ...calculate(state, e.target.innerText) });
   }
 
-  handler(e) {
-    this.setState((state) => calculate(state, e.target.innerText));
-  }
-
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div>
-        <div className="screen">
-          {
-            next || (total || '0')
-          }
-        </div>
-        <div className="calculatorKeys">
-          <button type="button" onClick={this.handler}>AC</button>
-          <button type="button" onClick={this.handler}>+/-</button>
-          <button type="button" onClick={this.handler}>%</button>
-          <button type="button" onClick={this.handler} className="orange">÷</button>
-          <button type="button" onClick={this.handler}>7</button>
-          <button type="button" onClick={this.handler}>8</button>
-          <button type="button" onClick={this.handler}>9</button>
-          <button type="button" onClick={this.handler} className="orange">x</button>
-          <button type="button" onClick={this.handler}>4</button>
-          <button type="button" onClick={this.handler}>5</button>
-          <button type="button" onClick={this.handler}>6</button>
-          <button type="button" onClick={this.handler} className="orange">-</button>
-          <button type="button" onClick={this.handler}>1</button>
-          <button type="button" onClick={this.handler}>2</button>
-          <button type="button" onClick={this.handler}>3</button>
-          <button type="button" onClick={this.handler} className="orange">+</button>
-          <button type="button" onClick={this.handler} className="zero">0</button>
-          <button type="button" onClick={this.handler}>.</button>
-          <button type="button" onClick={this.handler} className="orange">=</button>
-        </div>
+  const { next, total } = state;
+  return (
+    <div>
+      <div className="screen">
+        {
+          next || (total || '0')
+        }
       </div>
-    );
-  }
+      <div className="calculatorKeys">
+        <button type="button" onClick={handler}>AC</button>
+        <button type="button" onClick={handler}>+/-</button>
+        <button type="button" onClick={handler}>%</button>
+        <button type="button" onClick={handler} className="orange">÷</button>
+        <button type="button" onClick={handler}>7</button>
+        <button type="button" onClick={handler}>8</button>
+        <button type="button" onClick={handler}>9</button>
+        <button type="button" onClick={handler} className="orange">x</button>
+        <button type="button" onClick={handler}>4</button>
+        <button type="button" onClick={handler}>5</button>
+        <button type="button" onClick={handler}>6</button>
+        <button type="button" onClick={handler} className="orange">-</button>
+        <button type="button" onClick={handler}>1</button>
+        <button type="button" onClick={handler}>2</button>
+        <button type="button" onClick={handler}>3</button>
+        <button type="button" onClick={handler} className="orange">+</button>
+        <button type="button" onClick={handler} className="zero">0</button>
+        <button type="button" onClick={handler}>.</button>
+        <button type="button" onClick={handler} className="orange">=</button>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
