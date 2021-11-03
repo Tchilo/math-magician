241f8324f8b8ddc594b145597373ed934627e9eb
Tchilo <tchiloross@gmail.com>
2021-11-03 10:57:33 +0200

    Add Navbar file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Calculator from './components/calculator';
+import Navbar from './components/Navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,9 +10,12 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <Calculator />
-      </div>
+      <>
+        <Navbar />
+        <div className="App">
+          <Calculator />
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => (
+  <div>
+    <h1>Magic</h1>
+    <ul>
+      <li>Home</li>
+      <li>Calculator</li>
+      <li>Quote</li>
+    </ul>
+  </div>
+);
+
+export default Navbar;
