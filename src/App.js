de8e85bfa5f62ba32dd98f1eaf6a1d6ebba10877
Tchilo <tchiloross@gmail.com>
2021-11-03 17:49:10 +0200

    Fix eslinters

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+
 import Quote from './components/Quote';
 
 class App extends React.Component {
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="Home">
+    <div>
+      <h1>Welcome to my page!</h1>
+    </div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt dolore corrupti adipisci?
+      Nemo delectus, excepturi doloribus nam sint
+      fuga quasi atque debitis inventore amet nesciunt animi a asperiores et repudiandae!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt dolore corrupti adipisci?
+      Nemo delectus, excepturi doloribus nam sint
+      fuga quasi atque debitis inventore amet nesciunt animi a asperiores et repudiandae!
+    </p>
+  </div>
+);
+export default Home;
