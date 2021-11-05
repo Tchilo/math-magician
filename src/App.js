8a6eafc986d0b039c2c2e6f6da67c25ff82f5524
Tchilo <tchiloross@gmail.com>
2021-11-05 14:23:02 +0200

    Add test for operator functions

--- src/App.js
+++ src/App.js
@@ -3,35 +3,25 @@ import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-
 import Quote from './components/Quote';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Switch>
-          <Route path="/App">
-            <div className="App">
-              <Calculator />
-            </div>
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/Quote">
-            <Quote />
-          </Route>
-        </Switch>
-      </>
-    );
-  }
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Switch>
+      <Route path="/App">
+        <div className="App">
+          <Calculator />
+        </div>
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/Quote">
+        <Quote />
+      </Route>
+    </Switch>
+  </>
+);
 
 export default App;
---
+++ src/__tests__/Calculator.test.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom';
+import { render ,screen ,cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Calculator from '../components/calculator';
+
+describe('Calculator page render', () => {
+
+});
