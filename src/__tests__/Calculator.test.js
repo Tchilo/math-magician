ffcd2b36d8bf90d3908a1d248a217894910cf585
Taro Chilongoshi <Tchilo@users.noreply.github.com>
2021-11-05 23:43:49 +0200

    fix eslint errors

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 import { render, screen, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Calculator from '../components/calculator';
 
 describe('Calculator tests', () => {
